96809c9bfa5f0d7e737e7466e4fb02bd91466f38
Selmouni Abdelilah <123@gmail.com>
2023-12-15 22:41:01 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "rg" {
+}
+
+resource "azurerm_storage_account" "storage" {
+  
+}
+
+resource "azurerm_app_service_plan" "svcplan" {
+  
+}
+
+resource "azurerm_logic_app_standard" "logicapp" {
+  
+}
+resource "azurerm_logic_app_workflow" "workflow" {
+
+}
